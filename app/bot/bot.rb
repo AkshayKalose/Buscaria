68a4e06ddf864c944b8556a46794d20af1a7783f
Vishnu Sarukkai <vishnu.sarukkai@hotmail.com>
2017-02-18 17:00:42 -0800

    Ready to work on together

--- app/bot/bot.rb
+++ app/bot/bot.rb
@@ -3,73 +3,120 @@ include Facebook::Messenger
 Bot.on :message do |message|
   puts "Received '#{message.inspect}' from #{message.sender}"
 
-  # case message.sender
-    # check if in database
-  # end
+  if !User.exists?(id_fb:message.sender["id"])
+    # Create new user
+    user = User.create(id_fb: message.sender["id"])
+ #    message.reply(
+ #    	text: 'New user created!'
+	# )
+    # user.name_first = 'Vishnu'
+    # user.save
+  else
+  	user = User.where(id_fb: message.sender["id"]).first()
+ #  	message.reply(
+ #    	text: 'User already in database!'
+	# )
+  end
 
-  case message.text
-  when /help/i
-    message.reply(
+  # message.reply(
+  # 	text: "Id=" + message.sender["id"]
+  # 	)
+
+  # FIRST CHECK IF WE ARE ROUTING. HANDLE IF NOT ROUTING
+
+  if user.talking_to == nil || user.talking_to == 0
+	case message.text
+	when /help/i
+	message.reply(
 	  attachment: {
-        type: 'template',
-        payload: {
-          template_type: 'button',
-          text: 'Welcome to Buscaria! We connect people who want to learn a new language to real people who want to help them. Would you like to learn or teach?',
-          buttons: [
-            { type: 'postback', title: 'Learn!', payload: 'LANGUAGE_LEARN' },
-            { type: 'postback', title: 'Teach!', payload: 'LANGUAGE_TEACH' }
-          ]
-        }
-      }
-    )
-  when /\/practice ([a-zA-Z]*) (\d*)/i
+	    type: 'template',
+	    payload: {
+	      template_type: 'button',
+	      text: 'Welcome to Buscaria! We connect people who want to learn a new language to real people who want to help them. Would you like to learn or teach?',
+	      buttons: [
+	        { type: 'postback', title: 'Learn!', payload: 'LANGUAGE_LEARN' },
+	        { type: 'postback', title: 'Teach!', payload: 'LANGUAGE_TEACH' }
+	      ]
+	    }
+	  }
+	)
+	when /\/practice ([a-zA-Z]*) (\d*)/i
 
-  	var1 = $LAST_MATCH_INFO[1]
+	 # Check if anyone can help in database
 
-     # Check if anyone can help in database
+	 # if
 
-     # if
-
-    message.reply(
-      text: 'Great! Let\'s get started with ' + $1 + ' at level ' + $2
-    )
-
-     # else
-
-    message.reply(
-      text: 'Sorry, there is no one available to teach you ' + $1 + 'at level ' + $2
+	message.reply(
+	  text: 'Great! Let\'s get started with ' + $1 + ' at level ' + $2
 	)
 
-    # message.reply(
-    #   attachment: {
-    #     type: 'image',
-    #     payload: {
-    #       url: 'https://i.imgur.com/iMKrDQc.gif'
-    #     }
-    #   }
-    # )
+	 # else
 
-    # message.reply(
-    #   attachment: {
-    #     type: 'template',
-    #     payload: {
-    #       template_type: 'button',
-    #       text: 'Did human like it?',
-    #       buttons: [
-    #         { type: 'postback', title: 'Yes', payload: 'HUMAN_LIKED' },
-    #         { type: 'postback', title: 'No', payload: 'HUMAN_DISLIKED' }
-    #       ]
-    #     }
-    #   }
-    # )
+	message.reply(
+	  text: 'Sorry, there is no one available to teach you ' + $1 + 'at level ' + $2
+	)
+
+	# message.reply(
+	#   attachment: {
+	#     type: 'image',
+	#     payload: {
+	#       url: 'https://i.imgur.com/iMKrDQc.gif'
+	#     }
+	#   }
+	# )
+
+	# message.reply(
+	#   attachment: {
+	#     type: 'template',
+	#     payload: {
+	#       template_type: 'button',
+	#       text: 'Did human like it?',
+	#       buttons: [
+	#         { type: 'postback', title: 'Yes', payload: 'HUMAN_LIKED' },
+	#         { type: 'postback', title: 'No', payload: 'HUMAN_DISLIKED' }
+	#       ]
+	#     }
+	#   }
+	# )
+	end
   else
-    message.reply(
-      text: 'You are now marked for extermination.'
-    )
-
-    message.reply(
-      text: 'Have a nice day.'
-    )
+  	# Route!!
+  	# message.sender_id
+  	user2 = User.find(user.talking_to)
+  	case message.text
+  	when /\/exit/i
+  		# Disconnect both
+  		reply = {
+  			type: 'template',
+  			payload: {
+  				template_type: 'button',
+  				text: 'Your session has ended. How many karma points do you want to give your parter?',
+  				button: [
+  					{ type: 'postback', title: '1', payload: 'RATE_1_' + user2.id},
+  					{ type: 'postback', title: '2', payload: 'RATE_2_' + user2.id},
+  					{ type: 'postback', title: '3', payload: 'RATE_3_' + user2.id},
+  					{ type: 'postback', title: '4', payload: 'RATE_4_' + user2.id},
+  					{ type: 'postback', title: '5', payload: 'RATE_5_' + user2.id}
+  				]
+  			}
+  		}
+  		message.reply(reply)
+  		Bot.deliver({
+		  recipient: {
+		    id: user2.id_fb
+		  },
+		  message: reply,
+		}, access_token: ENV['ACCESS_TOKEN'])
+  	else
+	  	Bot.deliver({
+		  recipient: {
+		    id: user2.id_fb
+		  },
+		  message: {
+		  	text: message.text
+		  }
+		}, access_token: ENV['ACCESS_TOKEN'])
+  	end
   end
 end
 
